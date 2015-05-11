d880509aed3773616413229722ebfd654b8ca015
Casey Meisenzahl <caseymeiz@gmail.com>
2015-05-11 17:08:28 -0400

    add snake head, snake food. also made a grid of the board

--- snake.py
+++ snake.py
@@ -1,6 +1,12 @@
+# Name: Casey Meisenzahl
+# Date: 5/11/15
+# Program name: snake.py
+# Description: The Game snake, as you move around and eat blocks you grow larger. 
+#                  Try not to bite yourself or go out of bounds.
+
 import os
 import pickle
-import random
+from random import randint, randrange
 import tkinter as tk
 
 class Snake:
@@ -11,7 +17,6 @@ class Snake:
         self.BOREDER = 20
         self.background_color = 'lightblue'
         self.board_color = 'coral'
-        self.snake_color = '#3333ff'
         self.prey_color = '#ffff33'
 
         ## tk
@@ -25,11 +30,18 @@ class Snake:
         self.canvas.pack()
 
         self.draw_board()
+        self.grid = self.create_grid()
 
         self.banner = self.draw_banner()
         self.instructions = self.draw_instructions()
         self.current_score = self.draw_current_score()
         self.top_scores = self.draw_top_scores()
+
+        # the snake is made of blocks the first block in the list is the head
+        self.snake = []
+        self.add_snake_block(self.WIDTH*3//4,self.HEIGHT*2//3)
+        # the food is a single block
+        self.food = self.draw_food(self.WIDTH//4,self.HEIGHT*2//3)
 
 
     
@@ -39,6 +51,18 @@ class Snake:
                                      self.WIDTH-self.BOREDER,self.HEIGHT-self.BOREDER,
                                      fill=self.board_color,
                                      outline='')
+    def create_grid(self):
+        ''' a list of all the 20 by 20 squares on the board
+        param: none
+        return: list:
+        '''
+        grid = []
+        for x in range(self.BOREDER,self.WIDTH-self.BOREDER,20):
+            for y in range(self.BOREDER,self.HEIGHT-self.BOREDER,20):
+                grid.append([x,y])
+        return grid
+
+
 
     def draw_banner(self):
         '''Creates the banner, this this object will be used to display the title of the game,
@@ -108,6 +132,14 @@ class Snake:
                 high_score = [0,0,0,0,0]
                 pickle.dump(high_score,f)
 
+    def add_snake_block(self,x,y):
+        c = '123456789abcdef'
+        color = '#{}ff'.format(c[randint(0,14)],)
+        block_id = self.canvas.create_rectangle(x,y,x+20,y+20, fill=color,outline='')
+        self.snake.append(block_id)
+
+    def draw_food(self,x,y):
+        return self.canvas.create_rectangle(x,y,x+20,y+20, fill=self.prey_color,outline='')
 
 if __name__ == '__main__':
     Snake()
